4f8a75105a20b72f9e2dd99a9a1e129db0d9023a
Liz Lemon <eford0121@gmail.com>
2019-01-14 14:11:31 -0600

    committing

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -8,6 +8,7 @@ function buildMetadata(sample) {
     var panel = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
     panel.html("");
+    console.log(data);
 
     Object.entries(data).forEach(([key, value]) => {
       var paragraph = panel.append("p");
@@ -24,8 +25,6 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-    var pieChart = d3.select("#pie")
-    var bubbleChart = d3.select("#bubble")
     var url = "/samples/" + sample;
     // @TODO: Build a Bubble Chart using the sample data
     d3.json(url).then(function(data) {
@@ -36,35 +35,62 @@ function buildCharts(sample) {
       var trace1 = {
         x: sLabels,
         y: sValues,
-        mode: 'markers',
+        mode: "markers",
         text: sText,
         marker: {
           color: sLabels,
           size: sValues
         },
-        type: 'scatter'
+        type: "scatter"
       };
 
       var bubbledata = [trace1];
       var layout = {
-        xaxis: { title: "OTU ID"},
+        xaxis: { title: "OTU ID"}
       };
 
     Plotly.newPlot("bubble", bubbledata, layout);
+  });
+ ////////////////////////////////////////////////////////////////////////////
+    //build pie chart
+
+    d3.json(url).then((data)=>{
+      var sValues = data.sample_values.slice(0,9);
+      var sLabels = data.otu_ids.slice(0,9);
+      var sText = data.otu_labels.slice(0,9);
+
+      var trace2 = {
+        labels : sLabels,
+        values : sValues,
+        hovertext : sText,
+        hoverinfo: "hovertext",
+        type: "pie" 
+      };
+
+      var data2 = [trace2];
+      var layout2 = {
+        title: "Top 10 Samples",
+        autosize: true
+      };
+
+      Plotly.newPlot("pie", data2, layout2);
+
+    });
+
+  
+
+}
 
 
 
 
-      
-
-    // });
 
 
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-}
+
 
 function init() {
   // Grab a reference to the dropdown select element
@@ -81,14 +107,14 @@ function init() {
 
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
-    //buildCharts(firstSample);
+    buildCharts(firstSample);
     buildMetadata(firstSample);
   });
 }
 
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
-  //buildCharts(newSample);
+  buildCharts(newSample);
   buildMetadata(newSample);
 }
 
